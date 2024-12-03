ed137dafba969e1244465064191fff78c1e1fd35
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-12-02 23:07:56 -0300

    fix: get ventas

--- proyecto_tienda/src/main/java/com/example/demo/controller/ventas/VentasController.java
+++ proyecto_tienda/src/main/java/com/example/demo/controller/ventas/VentasController.java
@@ -38,28 +38,28 @@ public class VentasController {
 
 
 //    Hacer suma de  precio de todas las ventas de ese dia (ganancia) y ver cuantas compras hubo ese dia
-    @GetMapping("/get/{fecha_venta}")
-    public String getMonto_Fecha(@PathVariable String fecha_venta){
-        for(VentasEntity e : ven_serv.getVentas()){
-//            Comparo la fecha dentro de la lista de ventas y la fecha ingresada
-            if(e.getFecha_venta().equals(fecha_venta)){
-               String cantidad_compras = "Hubo esta cantidad de compras este dia: ";
-               int longitud_ventas =  e.getLista_prod().size();
-
-                Double ganancia = null;
-//                Acá busco en la lista de productos de la entidad productos
-               for (ProductoEntity i  : prod_serv.getProductos()) {
-//                   Trato de sumar la ganancia de ese dia
-                    ganancia += i.getCosto();
-                    return "Ganancia: " +  ganancia;
-               }
-               return cantidad_compras + longitud_ventas ;
-            }
-
-        }
-        return "fdmkvmkdf";
-
-    }
+//    @GetMapping("/get/fecha/{fecha_venta}")
+//    public String getMonto_Fecha(@PathVariable String fecha_venta){
+//        for(VentasEntity e : ven_serv.getVentas()){
+////            Comparo la fecha dentro de la lista de ventas y la fecha ingresada
+//            if(e.getFecha_venta().equals(fecha_venta)){
+//               String cantidad_compras = "Hubo esta cantidad de compras este dia: ";
+//               int longitud_ventas =  e.getLista_prod().size();
+//
+//                Double ganancia = null;
+////                Acá busco en la lista de productos de la entidad productos
+//               for (ProductoEntity i  : prod_serv.getProductos()) {
+////                   Trato de sumar la ganancia de ese dia
+//                    ganancia += i.getCosto();
+//                    return "Ganancia: " +  ganancia;
+//               }
+//               return cantidad_compras + longitud_ventas ;
+//            }
+//
+//        }
+//        return "fdmkvmkdf";
+//
+//    }
 
     @PostMapping("/post")
     public ResponseEntity<?>  postVentas(@RequestBody VentasEntity e){
